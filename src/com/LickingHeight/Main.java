558f635379d7c35dbe6eefe74b9acfef73f66a9b
Amber <amber.ramey.2022@student.lhschools.org>
2020-03-06 11:57:01 -0500

    intelij commit

---
+++ src/com/LickingHeight/Main.java
@@ -0,0 +1,16 @@
+package com.LickingHeight;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.println("Amber");
+
+        //print three more lines about what you did last summer
+        System.out.println("I played softball last Summer");
+        System.out.println("I hung out with friends a lot");
+        System.out.println("I got a new dog");
+
+    }
+}
